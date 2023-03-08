d3a976e87293b84420b7563f075f34e24171129a
Pushpanjali500 <kumpushpanjali002@gmail.com>
2023-03-08 23:06:52 +0530

    subarray

---
+++ array/medium/subarray_with_given_sum.cpp
@@ -0,0 +1,85 @@
+/*
+Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
+
+In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
+
+Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
+
+Example 1:
+
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+ 
+
+Example 2:
+
+Input:
+N = 10, S = 15
+A[] = {1,2,3,4,5,6,7,8,9,10}
+Output: 1 5
+Explanation: The sum of elements 
+from 1st position to 5th position
+is 15.
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {if(s==0) return {-1};
+       int sum = 0;
+       int i = 0;
+       int j = 0;
+       while(j<n){
+           if(sum<s){
+               sum+=arr[j];
+           }
+           while(sum>s){
+               sum-=arr[i];
+               i++;
+           }
+           if(sum == s){
+               return {i+1 , j+1};
+           }
+           j++;
+       }
+       return {-1};
+    }
+};
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
